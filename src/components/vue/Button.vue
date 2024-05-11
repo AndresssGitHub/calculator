1b147ebdc75efb0ab868eb2773d1723d2b77f8da
Andresss <davaccar@ufpso.edu.co>
2024-05-11 11:15:36 -0500

    Se completó la estructura

--- src/components/vue/Button.vue
+++ src/components/vue/Button.vue
@@ -1,7 +1,9 @@
 <script setup>
-    
+    import { ref } from "vue";
 </script>
 
 <template>
-    
+    <button class="bg-orange-400 rounded-md w-12 h-12 font-medium text-white">
+        7
+    </button>
 </template>
